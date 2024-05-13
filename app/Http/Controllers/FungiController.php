de3d330da48f7eb1244b8e0c74576dd700f0db4a
Pedro-Benevides <pvsbenevides197@gmail.com>
2024-05-13 00:03:34 -0300

    fix(FungiController): ajusta acesso a atributos validados da request

--- app/Http/Controllers/FungiController.php
+++ app/Http/Controllers/FungiController.php
@@ -4,29 +4,29 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ListFungiRequest;
 use App\Services\Contracts\FungiContract;
-use App\Services\FungiService;
+// use App\Services\FungiService;
 use Illuminate\Http\Request;
 
 class FungiController extends Controller
 {
-    private FungiService $service;
+    private FungiContract $service;
 
-    public function __construct()
+    public function __construct(FungiContract $service)
     {
-        $this->service = FungiContract::class;
+        $this->service = $service;
     }
 
     public function getByTaxonomy(ListFungiRequest $request)
     {
-        $request->validated();
+        $data = $request->validated();
 
-        $taxonomy = $request->taxonomy;
-        $stateAc = $request->stateAc;
-        $bem = $request->bem;
-        $biome = $request->biome;
+        $taxonomy = $data['taxonomy'];
+        $stateAc = array_key_exists('stateAc', $data) ? $data['stateAc'] : null;
+        $bem = array_key_exists('bem', $data) ? $data['bem'] : null;
+        $biome = array_key_exists('biome', $data) ? $data['biome'] : null;
 
         try {
-            return $this->service->getByTaxonomy($taxonomy, $stateAc, $biome, $bem)->toArray();
+            return $this->service->getByTaxonomy($taxonomy, $stateAc, $biome, $bem);
         } catch (\Throwable $th) {
             throw $th;
         }
