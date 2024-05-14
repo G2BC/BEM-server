6b671b04838995c11b3a063dba17701106966a39
Uendel Lima <uendellima54@gmail.com>
2024-05-13 23:47:35 -0300

    feat(FungiByBemList): consulta de fungos por classificação BEM

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'fungi'], function () {
     Route::get('/taxonomy', 'FungiController@getByTaxonomy');
     Route::get('/stateAc/{stateAc}', 'FungiController@getByStateAc');
+    Route::get('/bem/{id}', 'FungiController@getByBem');
 });
 
