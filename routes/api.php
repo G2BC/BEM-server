fef75ac927974d889ace847df9e728bb4b085e88
Uendel Lima <uendellima54@gmail.com>
2024-05-13 22:40:39 -0300

    feat(api): implementa endpoint get para consulta de fungos por estado

--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'fungi'], function () {
     Route::get('/taxonomy', 'FungiController@getByTaxonomy');
+    Route::get('/stateAc/{stateAc}', 'FungiController@getByStateAc');
 });
+
