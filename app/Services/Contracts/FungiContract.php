dbe1b4ef8734887e3bf65327974590cee16362c6
Uendel Lima <uendellima54@gmail.com>
2024-05-19 22:43:48 -0300

    feature: implementa paginação no método getByTaxonomy

--- app/Services/Contracts/FungiContract.php
+++ app/Services/Contracts/FungiContract.php
@@ -5,11 +5,12 @@ namespace App\Services\Contracts;
 use App\Services\Contracts\Contract;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Collection as SupportCollection;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 interface FungiContract extends Contract
 {
 
-    public function getByTaxonomy(string $taxonomy, string|null $occurrenceStateAcronym, string|null $biome, int|null $bemClassification): Collection;
+    public function getByTaxonomy(string $taxonomy, string|null $occurrenceStateAcronym, string|null $biome, int|null $bemClassification, int|null $page): LengthAwarePaginator;
 
     public function getByStateAc(string $occurrenceStateAcronym): Collection;
 
