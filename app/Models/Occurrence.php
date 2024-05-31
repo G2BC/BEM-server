9f838085ca497dcce23de880118219de5ab69fdf
Pedro-Benevides <pvsbenevides197@gmail.com>
2024-05-31 10:45:23 -0300

    chore(Occurrence): adiciona campo de curadoria

--- app/Models/Occurrence.php
+++ app/Models/Occurrence.php
@@ -6,6 +6,7 @@
 
 namespace App\Models;
 
+use App\Utils\Enums\OccurrenceTypes;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -17,16 +18,17 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  * @property uuid $uuid
  * @property int|null $inaturalist_taxa
  * @property int|null $specieslink_id
- * @property int $type
+ * @property OccurrenceTypes $type
  * @property string $state_acronym
  * @property string $state_name
  * @property string $biome
  * @property string $literature_reference
  * @property float|null $latitude
  * @property float|null $longitude
+ * @property bool $curation
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
- * @property string|null $deleted_at
+ * @property Carbon|null $deleted_at
  *
  * @package App\Models
  */
@@ -39,7 +41,7 @@ class Occurrence extends Model
 		'uuid' => 'string',
 		'inaturalist_taxa' => 'int',
 		'specieslink_id' => 'int',
-		'type' => 'int',
+		'type' => OccurrenceTypes::class,
 		'latitude' => 'double',
 		'longitude' => 'double'
 	];
@@ -54,10 +56,16 @@ class Occurrence extends Model
 		'habitat',
 		'literature_reference',
 		'latitude',
-		'longitude'
+		'longitude',
+		'curation'
 	];
 
-	protected $guarded = ['id'];
+	protected $guarded = [
+		'id',
+		'created_at',
+		'updated_at',
+		'deleted_at'
+	];
 
 	public function fungis()
 	{
